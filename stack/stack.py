4a9a362c01cb2797c9990c6a4f0c2c5fc0cfb877
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-08-10 19:25:30 -0400

    open pull request

--- stack/stack.py
+++ stack/stack.py
@@ -13,13 +13,14 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
 
     def push(self, value):
         pass
 
     def pop(self):
         pass
+
+    
